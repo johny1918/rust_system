fecfbbe3a148f49eea33a7643804a94b8a0cdfaa
Ioan Andrasi <andrasi.ioan90@gmail.com>
2026-01-17 20:43:50 +0200

    generic function

--- rust_in_action/src/chapter_two.rs
+++ rust_in_action/src/chapter_two.rs
@@ -2,3 +2,8 @@
 pub fn add_with_lifetimes<'a, 'b>(i: &'a i32, j: &'b i32) -> i32 {
     *i + *j
 }
+
+// Generic function
+pub fn add<T: std::ops::Add<Output = T>>(i: T, j: T) -> T {
+    i + j
+}
--- rust_in_action/src/main.rs
+++ rust_in_action/src/main.rs
@@ -1,10 +1,14 @@
 mod chapter_two;
 use crate::chapter_two::add_with_lifetimes;
+use crate::chapter_two::add;
 
 fn main() {
     let a = 2;
     let b= 3;
 
     let sum = add_with_lifetimes(&a, &b);
+    println!("Sum: {}", sum);
+
+    let sum = add(a, b);
     println!("Sum: {}", sum);
 }
